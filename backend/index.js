13a9bc8c12a699bb0e5faedca919bf1787bb1593
Oona Saksman <oona.saksman@tuni.fi>
2023-01-14 21:54:58 +0200

    Implement API call for drone info with axios.

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,7 @@
 const path = require('path');
 const express = require("express");
 var axios = require('axios');
+const request = require('request');
 
 const PORT = process.env.PORT || 3001;
 
@@ -11,25 +12,53 @@ const app = express();
 // Have Node serve the files for our built React app
 app.use(express.static(path.resolve(__dirname, '../frontend/build')));
 
+// app.get("/api", (req, res) => {
+
+// var request = require('request');
+// var options = {
+//   'method': 'GET',
+//   'url': 'http://assignments.reaktor.com/birdnest/drones',
+//   'headers': {
+//   }
+// };
+
+// request(options, function (error, response) {
+
+//   // var xml = require('xml');
+//   if (error){
+//     res.status(404).write("NO LUCK"); 
+//     throw new Error(error);
+//   } 
+//   // res.text(response.body);
+//   // res.set({ 'content-type': 'application/xml; charset=utf-8' });
+//   res.set("Content-Type", "application/xml");
+//   // res.set("xml", response.body);
+//   // res.send(xml(response.body));
+//   // res.type('application/xml');
+//   res.status(200).write(response.body);
+// });
+
+// });
 
 app.get("/api", (req, res) => {
 
-    var axios = require('axios');
+var axios = require('axios');
 
-    var config = {
-      method: 'get',
-      url: 'http://assignments.reaktor.com/birdnest/drones',
-      headers: { }
-    };
-    
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-      res.json({message: JSON.stringify(response.data) });
-    })
-    .catch(function (error) {
-      console.log(error);
-    });
+var config = {
+  method: 'get',
+  url: 'http://assignments.reaktor.com/birdnest/drones',
+  headers: {'Content-Type': 'application/xml'}
+};
+
+axios(config)
+.then(function (response) {
+  res.json(response.data)
+  console.log(response.data);
+})
+.catch(function (error) {
+  console.log(error);
+});
+
 });
 
 app.listen(PORT, () => {
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,21 +1,63 @@
-import logo from './logo.svg';
 import './App.css';
 import {useEffect, useState} from 'react';
+import XMLParser from 'react-xml-parser';
+import axios from "axios";
 
 function App() {
-  const [data, setData] = useState(null);
+  const [drones, setDrones] = useState(null);
+  
+   useEffect(() => {
 
-  useEffect(() => {
-    fetch("/api")
-      .then((res) => res.json())
-      .then((data) => setData(data.message));
+    // //logs nothing
+    // fetch("/api", {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/xml',
+    //   }
+    // })
+    //   .then((res) => res.text())
+    //   .then((data) => console.log(data.body))
+
+    // //logs nothing
+    // fetch("/api")
+    // .then(res => res.text())
+    // .then(data => {
+    //     var xml = new XMLParser().parseFromString(data); 
+    //     console.log(xml)
+    // })
+    // .catch(err => console.log(err));
+
+    // //logs readablestream, just res logs response 
+    //   fetch("/api")
+    //  .then((res) => res.body)
+    //  .then((data) => console.log(data));
+
+    //hurrah
+    axios.get("/api")
+    //.then((res) => res.text())
+    .then((data) => console.log(data.data));
+    //});
+
+    
   }, []);
+
+      // fetch("/api")
+    //   .then((res) => res.json())
+    //   .then((data) => console.log(data.message));
+
+  const parseXml = (drones) => {
+    const doc = new DOMParser().parseFromString(drones, "text/xml");
+    const xml = doc.firstChild;
+    return xml;
+  }
+
+        // <p>{!drones ? "Loading..." : drones}</p>
+        // {drones ? console.log(parseXml(drones)) : ""}
 
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{!data ? "Loading..." : data}</p>
+        
       </header>
     </div>
   );
