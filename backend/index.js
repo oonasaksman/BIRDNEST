68a8434e33f261688a162b8c8678569dabb386a2
Oona Saksman <oona.saksman@tuni.fi>
2023-01-18 01:14:28 +0200

    Implement function to retrieve date, add setInterval and save incoming drone data to state.

--- backend/index.js
+++ backend/index.js
@@ -1,75 +1,41 @@
 // server/index.js
-const path = require('path');
+const path = require("path");
 const express = require("express");
-var bodyParser = require('body-parser');
-var axios = require('axios');
-const request = require('request');
+var bodyParser = require("body-parser");
+var axios = require("axios");
+const request = require("request");
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-
 // Have Node serve the files for our built React app
-app.use(express.static(path.resolve(__dirname, '../frontend/build')));
-
-// app.get("/api", (req, res) => {
-
-// var request = require('request');
-// var options = {
-//   'method': 'GET',
-//   'url': 'http://assignments.reaktor.com/birdnest/drones',
-//   'headers': {
-//   }
-// };
-
-// request(options, function (error, response) {
-
-//   // var xml = require('xml');
-//   if (error){
-//     res.status(404).write("NO LUCK"); 
-//     throw new Error(error);
-//   } 
-//   // res.text(response.body);
-//   // res.set({ 'content-type': 'application/xml; charset=utf-8' });
-//   res.set("Content-Type", "application/xml");
-//   // res.set("xml", response.body);
-//   // res.send(xml(response.body));
-//   // res.type('application/xml');
-//   res.status(200).write(response.body);
-// });
-
-// });
-
+app.use(express.static(path.resolve(__dirname, "../frontend/build")));
 
 //TODO: ADD ERROR CODES
 app.get("/api/drones", (req, res) => {
+  var axios = require("axios");
 
-var axios = require('axios');
+  var config = {
+    method: "get",
+    url: "http://assignments.reaktor.com/birdnest/drones",
+    headers: { "Content-Type": "application/xml" },
+  };
 
-var config = {
-  method: 'get',
-  url: 'http://assignments.reaktor.com/birdnest/drones',
-  headers: {'Content-Type': 'application/xml'}
-};
-
-axios(config)
-.then(function (response) {
-  res.json(response.data)
-  console.log(response.data);
-})
-.catch(function (error) {
-  console.log(error);
+  axios(config)
+    .then(function (response) {
+      res.json(response.data);
+      console.log(response.data);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
 });
-
-});
-
-
 
 app.post("/api/users", (req, res) => {
-
-  var axios = require('axios');
+  var axios = require("axios");
   var dataObj = {};
+  res.header({ "Access-Control-Allow-Origin": "*" });
 
   const chunks = [];
   req.on("data", (chunk) => {
@@ -86,47 +52,37 @@ app.post("/api/users", (req, res) => {
     }
 
     var config = {
-      method: 'get',
-      url: 'http://assignments.reaktor.com/birdnest/pilots/' + dataObj.serialNumber,
-      headers: {'Content-Type': 'application/json'}
+      method: "get",
+      url:
+        "http://assignments.reaktor.com/birdnest/pilots/" +
+        dataObj.serialNumber,
+      validateStatus: () => true,
+      headers: { "Content-Type": "application/json" },
     };
-  
+
     axios(config)
-    .then(function (response) {
-      res.json(response.data)
-      console.log(response.data);
-    })
-    .catch(function (error) {
-      console.log("404");
-      console.log('http://assignments.reaktor.com/birdnest/pilots/' + dataObj.serialNumber);
-    });
-
-  })
-  
-
-  // var config = {
-  //   method: 'get',
-  //   url: 'http://assignments.reaktor.com/birdnest/pilots/' + dataObj.serialNumber,
-  //   headers: {'Content-Type': 'application/json'}
-  // };
-
-  // axios(config)
-  // .then(function (response) {
-  //   res.json(response.data)
-  //   console.log(response.data);
-  // })
-  // .catch(function (error) {
-  //   console.log("404");
-  // });
-  
+      .then(function (response) {
+        res.json(response.data);
+        console.log(response.data);
+        res.status(200).end();
+      })
+      .catch(function (error) {
+        console.log(error);
+        //console.log("errorbutwontsay: ", response.data);
+        console.log(
+          "http://assignments.reaktor.com/birdnest/pilots/" +
+            dataObj.serialNumber
+        );
+        res.status(404).end();
+      });
   });
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
 
 // All other GET requests not handled before will return our React app
-app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../frontend/build', 'index.html'));
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/build", "index.html"));
 });
-  
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,174 +1,198 @@
-    // //logs nothing
-    // fetch("/api", {
-    //   method: 'GET',
-    //   headers: {
-    //     'Content-Type': 'application/xml',
-    //   }
-    // })
-    //   .then((res) => res.text())
-    //   .then((data) => console.log(data.body))
-
-    // //logs nothing
-    // fetch("/api")
-    // .then(res => res.text())
-    // .then(data => {
-    //     var xml = new XMLParser().parseFromString(data); 
-    //     console.log(xml)
-    // })
-    // .catch(err => console.log(err));
-
-    // //logs readablestream, just res logs response 
-    //   fetch("/api")
-    //  .then((res) => res.body)
-    //  .then((data) => console.log(data));
-
-          // fetch("/api")
-    //   .then((res) => res.json())
-    //   .then((data) => console.log(data.message));
-
-
-import './App.css';
-import {useEffect, useState} from 'react';
-import XMLParser from 'react-xml-parser';
+import "./App.css";
+import { useEffect, useState } from "react";
+import XMLParser from "react-xml-parser";
 import axios from "axios";
-import Decimal from 'decimal.js';
+import Decimal from "decimal.js";
 import qs from "qs";
 
 function App() {
-  const [drones, setDrones] = useState(null);
+  //const [drones, setDrones] = useState(null);
   const [closeDrones, setCloseDrones] = useState({});
-  const [positionY, setPositionY] = useState([]);
-  const [positionX, setPositionX] = useState([]);
-  const [serialNumbers, setSerialNumbers] = useState([]);
-  
-   useEffect(() => {
+  // const [positionY, setPositionY] = useState([]);
+  // const [positionX, setPositionX] = useState([]);
+  // const [serialNumbers, setSerialNumbers] = useState([]);
 
-    axios.get("/api/drones")
-    //.then((res) => res.text())
-    .then(droneXml => {
-        console.log(droneXml.data);
-        setDrones(droneXml.data);
-        const numArray = parseXml(droneXml.data);
-        console.log(parseXml(droneXml.data));
-        const eka = Number(numArray[0]);
-        const tok = Number(numArray[1]);
-        console.log(droneOwner("SN-jFpmj52QAj"));
-        //console.log(zoneCalculator(parseXml(droneXml.data)));
-    })
-    
+  useEffect(() => {
+    let id = setInterval(getDrones, 5000);
+    return () => clearInterval(id);
   }, []);
 
+  const getDrones = () => {
+    axios
+      .get("/api/drones")
+      //.then((res) => res.text())
+      .then((droneXml) => {
+        console.log(droneXml.data);
+        parseXml(droneXml.data);
+        //setDrones(droneXml.data);
+        //const numArray = parseXml(droneXml.data);
+        //console.log(parseXml(droneXml.data));
+        // const eka = Number(numArray[0]);
+        // const tok = Number(numArray[1]);
+
+        //console.log(zoneCalculator(parseXml(droneXml.data)));
+      });
+  };
 
   const parseXml = (drones) => {
-    const doc = new DOMParser().parseFromString(drones, "text/xml");
-    const xml = doc.firstChild;
-    const droneList = xml.getElementsByTagName("drone");
-    const time = xml.getElementsByTagName("deviceStarted");
-    console.log(time[0].innerHTML);
+    let doc = new DOMParser().parseFromString(drones, "text/xml");
+    let xml = doc.firstChild;
+    let droneList = xml.getElementsByTagName("drone");
+    let date = getDate();
     console.log(droneList);
     let arrY = [];
-    for(let x = 0; x < droneList.length; x++){
-      arrY.push(droneList[x].childNodes[15].innerHTML)
+    for (let x = 0; x < droneList.length; x++) {
+      arrY.push(droneList[x].childNodes[15].innerHTML);
     }
-    setPositionY(arrY);
+    //setPositionY(arrY);
     let arrX = [];
-    for(let x = 0; x < droneList.length; x++){
-      arrX.push(droneList[x].childNodes[17].innerHTML)
+    for (let x = 0; x < droneList.length; x++) {
+      arrX.push(droneList[x].childNodes[17].innerHTML);
     }
-    setPositionX(arrX);
+    //setPositionX(arrX);
     let arrS = [];
-    for(let x = 0; x < droneList.length; x++){
-      arrS.push(droneList[x].childNodes[1].innerHTML)
+    for (let x = 0; x < droneList.length; x++) {
+      arrS.push(droneList[x].childNodes[1].innerHTML);
     }
 
-    for(let x = 0; x < droneList.length; x++){
-      //if drone is too close and drone not already in json
-      if(isClose(arrX[x], arrY[x]) && !closeDrones[arrS[x]] ){
-        const ownerInfo = droneOwner();
-        closeDrones[arrS[x]] = arrX[x];
-        closeDrones[arrS[x]] = arrY[x];
-        closeDrones[arrS[x]] = ownerInfo[0]; //name
-        closeDrones[arrS[x]] = ownerInfo[1]; //phone
-        closeDrones[arrS[x]] = ownerInfo[2]; //email
+    for (let x = 0; x < droneList.length; x++) {
+      //if drone is too close and not already in json
+      if (isClose(arrX[x], arrY[x]) && !closeDrones[arrS[x]]) {
+        //for(let y = 0; y < arrS.length; y++){
+        droneOwner(arrS[x]).then((result) => {
+          let update = {};
+          update = {
+            [arrS[x]]: {
+              positionX: arrX[x],
+              positionY: arrY[x],
+              time: date,
+              firstName: result.firstName,
+              lastName: result.lastName,
+              email: result.email,
+              phoneNumber: result.phoneNumber,
+            },
+          };
+          setCloseDrones((closeDrones) => ({
+            ...update,
+            ...closeDrones,
+          }));
+          console.log(result, [arrS[x]]);
+          // setCloseDrones({
+          //   [arrS[x]]: {
+          //               positionX: arrX[x],
+          //               positionY: arrY[x],
+          //               time: time[0].innerHTML,
+          //               firstName: result.firstName,
+          //               lastName: result.lastName,
+          //               email: result.email,
+          //               phoneNumber: result.phoneNumber
+          //               }});
+          //setCloseDrones({meeps: "meep morp", ...closeDrones})
+          console.log("too close");
+          console.log(JSON.stringify(closeDrones));
+        });
+        //}
       }
       //too close but already in json
-      if(isClose(arrX[x], arrY[x]) && closeDrones[arrS[x]]
-        && howFar(arrX[x], arrY[x] < closeDrones[arrS[x]] )
-      ){
-        
+      else if (
+        isClose(arrX[x], arrY[x]) &&
+        closeDrones[arrS[x]] &&
+        howFar(arrX[x], arrY[x]) <
+          howFar(closeDrones[arrS[x]][0], closeDrones[arrS[x]][1])
+      ) {
       }
-
     }
     //console.log(isClose(188106.66769, 171648.743947))
     return arrX;
-  }
+  };
 
+  const getDate = () => {
+    var today = new Date(),
+      date =
+        today.getHours() +
+        "." +
+        today.getMinutes() +
+        "-" +
+        today.getDate() +
+        "." +
+        (today.getMonth() + 1) +
+        "." +
+        today.getFullYear();
+    console.log(date);
+    return date;
+  };
+
+  //calculates distance from nest to drone
   const howFar = (coX, coY) => {
-    const minus = coY - 250000;
-    const minus2 = coX - 250000;
-    const power = Math.pow(minus, 2);
-    const power2 = Math.pow(minus2, 2);
-    const sum = power + power2;
-    const distance = Math.sqrt(sum);
+    let minus = coY - 250000;
+    let minus2 = coX - 250000;
+    let power = Math.pow(minus, 2);
+    let power2 = Math.pow(minus2, 2);
+    let sum = power + power2;
+    let distance = Math.sqrt(sum);
     return distance;
-  }
+  };
 
   //returns true if drone is too close
   const isClose = (coX, coY) => {
-    const radius = 100000.0000;
-    const distance = howFar(coX, coY);
-    if(distance < radius){
+    const radius = 100000.0;
+    let distance = howFar(coX, coY);
+    if (distance < radius) {
       return true;
     }
     return false;
-  }
+  };
 
+  // const forLoop = async () => {
 
-  const droneOwner = (serialNumber) => {
+  //   for (let index = 0; index < fruitsToGet.length; index++) {
+  //     const fruit = fruitsToGet[index]
+  //     const numFruit = await getNumFruit(fruit)
+  //     console.log(numFruit)
+  //   }
+  // }
 
-    // var config = {
-    //   method: 'post',
-    //   url: '/api/users',
-    //   headers: {'Content-Type': 'application/json'},
-    //   data: {"serialNumber": serialNumber}
-    // };
-
-    // axios(config)
-    // .then(userInfo => {
-    //     console.log(userInfo.data);
-    //     return userInfo.data;
-    // })
-
-    
+  const droneOwner = async (serialNumber) => {
     var data = qs.stringify({
-      'serialNumber': 'SN-XXQq3bSWY7' 
+      serialNumber: serialNumber,
     });
     var config = {
-      method: 'post',
-      url: 'http://localhost:3001/api/users',
-      headers: { 
-        'Content-Type': 'application/x-www-form-urlencoded'
+      method: "post",
+      url: "http://localhost:3001/api/users",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
       },
-      data : data
+      data: data,
     };
 
-    axios(config)
-    .then(function (response) {
-      console.log(JSON.stringify(response.data));
-    })
-    .catch(function (error) {
+    try {
+      const response = await axios(config);
+      return response.data;
+    } catch (error) {
       console.log("404");
-    });
+      return "could not retrieve owner info";
+    }
+  };
 
-  }
-
-        // <p>{!drones ? "Loading..." : drones}</p>
-        // {drones ? console.log(parseXml(drones)) : ""}
+  // <p>{!drones ? "Loading..." : drones}</p>
+  // {drones ? console.log(parseXml(drones)) : ""}
 
   return (
     <div className="App">
       <header className="App-header">
-        
+        <div>
+          {Object.keys(closeDrones).map((item, i) => (
+            <li className="travelcompany-input" key={i}>
+              <span className="input-label">
+                {closeDrones[item].lastName + "----"}
+              </span>
+              <span className="input-label">
+                {closeDrones[item].time + "----"}
+              </span>
+              <span className="input-label">{closeDrones[item].positionX}</span>
+            </li>
+          ))}
+        </div>
       </header>
     </div>
   );
